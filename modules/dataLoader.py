97a7aaeda9284e04d9739340670548ccfcb94b48
Yihsun Ethan Cheng <33135703+YihsunEthanCheng@users.noreply.github.com>
2018-12-04 00:17:34 -0800

    Test data generator + renamed a file

--- modules/dataLoader.py
+++ modules/dataLoader.py
@@ -14,22 +14,27 @@ from sklearn.utils import shuffle
 
 #%%
 
-df = pd.read_csv('./data/driving_log.csv')
-df_train, df_valid = train_test_split(df, test_size=0.2)
-
+def df2xy(dff, dataPath = 'data/'):
+    images = []
+    for i in range(len(dff)):
+        images.append(cv2.imread(dataPath + dff.center.iloc[i]))
+    return np.array(images), np.array(dff.steering)
 
 def generator(df, batch_size=32, dataPath = 'data/'):
     while 1: # Loop forever so the generator never terminates
         shuffle(df)
         for offset in range(0, len(df), batch_size):
-            batch = df[offset:offset+batch_size]
-            images = []
-            for i in range(len(batch)):
-                images.append(cv2.imread(dataPath + batch.center[i]))
-            # trim image to only see section with road
-            X_train = np.array(images)
-            y_train = np.array(batch.steering)
+            df_i = df[offset:offset+batch_size]
+            X_train, y_train = df2xy(df_i, dataPath)
             yield shuffle(X_train, y_train)
+
+#%%
+df = pd.read_csv('./data/driving_log.csv')
+df_train, df_valid = train_test_split(df, test_size=0.2)
+
+X_valid, y_valid =  df2xy(df_valid[:10])
 
 # compile and train the model using the generator function
 train_generator = generator(df_train, batch_size=32)
+
+X_train, y_train = next(train_generator)

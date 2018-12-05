cc65bdd44ddc54cb3fafb78397d0d95e3ea37bcf
Yihsun Ethan Cheng <33135703+YihsunEthanCheng@users.noreply.github.com>
2018-12-04 22:40:38 -0800

    Encapsulated data into a class

--- modules/dataLoader.py
+++ modules/dataLoader.py
@@ -12,29 +12,32 @@ import numpy as np
 import pandas as pd
 from sklearn.utils import shuffle
 
-#%%
 
-def df2xy(dff, dataPath = 'data/'):
-    images = []
-    for i in range(len(dff)):
-        images.append(cv2.imread(dataPath + dff.center.iloc[i]))
-    return np.array(images), np.array(dff.steering)
+class behaviorCloneData(object):
+    
+    def __init__(self, path ):
+        self.path = path + '/'
+        self.df = pd.read_csv(self.path + 'driving_log.csv')
+        self.df_train, self.df_valid = train_test_split(self.df, test_size=0.2)
+        self.x_valid, self.y_valid = self.df2xy(self.df_valid)
+        
+    def df2xy(self, dff):
+        images = []
+        for i in range(len(dff)):
+            images.append(cv2.imread(self.path + dff.center.iloc[i]))
+        return np.array(images), np.array(dff.steering)
+    
+    def batchGenerator(self, batch_size=32):
+        while 1: # Loop forever so the generator never terminates
+            shuffle(self.df_train)
+            for offset in range(0, len(self.df_train), batch_size):
+                df_i = self.df_train[offset:offset+batch_size]
+#                X_train, y_train = self.df2xy(df_i)
+#                yield shuffle(X_train, y_train)
+                yield shuffle(self.df2xy(df_i))
 
-def generator(df, batch_size=32, dataPath = 'data/'):
-    while 1: # Loop forever so the generator never terminates
-        shuffle(df)
-        for offset in range(0, len(df), batch_size):
-            df_i = df[offset:offset+batch_size]
-            X_train, y_train = df2xy(df_i, dataPath)
-            yield shuffle(X_train, y_train)
-
-#%%
-df = pd.read_csv('./data/driving_log.csv')
-df_train, df_valid = train_test_split(df, test_size=0.2)
-
-X_valid, y_valid =  df2xy(df_valid[:10])
-
-# compile and train the model using the generator function
-train_generator = generator(df_train, batch_size=32)
-
-X_train, y_train = next(train_generator)
+    def validSet(self):
+        return self.x_valid, self.y_valid
+        
+        
+        

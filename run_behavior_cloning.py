30eb16521095228d36e1167d4aa20203a188f4d6
Yihsun Ethan Cheng <33135703+YihsunEthanCheng@users.noreply.github.com>
2018-12-07 01:02:41 -0800

    Changed output dimension to 1

--- run_behavior_cloning.py
+++ run_behavior_cloning.py
@@ -10,7 +10,6 @@ Created on Tue Dec  4 22:01:42 2018
 from modules.dataLoader import behaviorCloneData
 from modules.roboPilot import pilot
 
-#%%
 params = {
         'input_shape':  (160,320, 3),
         'cropping': ((50,20), (0,0)),
@@ -20,17 +19,17 @@ params = {
         'convDropout' : 0.2,
         'FC': [256, 64],
         'fcDropout': 0.5,
-        'outDim': 10,
+        'outDim': 1,
         'batch_size' : 128
         }
-        
-#%%
-data = behaviorCloneData('data/download')
 
+data = behaviorCloneData('data/download')
 
 #%%
 model = pilot(params)
 
-#%% 
-model.train(data, 1)
+#%%
+
+yhat = model.predict(data.x_valid,  verbose=1)
+
 
